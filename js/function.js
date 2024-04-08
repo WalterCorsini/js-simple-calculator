f51093dbc7e022d56475a19b7a2661c40dbd55ce
WalterCorsini <corsini.walter@gmail.com>
2024-04-08 15:08:56 +0200

    add: create symbol with  function

--- js/function.js
+++ js/function.js
@@ -1,3 +1,8 @@
+/**
+ * create button number
+ * @param {number} number
+ * @returns {object} element HTML
+ */
 function createNumber(number){
     const divElem = document.createElement("div");
     divElem.classList.add("numbers");
@@ -12,7 +17,46 @@ function createNumber(number){
         divElem.classList.add("bg-orange");
         divElem.innerText = "=";      
     }
+
     // divElem.addEventListener("click", operation);
     return divElem;
 }
 
+/**
+ * create button operations
+ * @param {number} numberSymbol
+ * @returns {object}  element HTML
+ */
+function createSymbol(numberSymbol){
+    const divElem = document.createElement("div");
+    divElem.classList.add("symbols");
+    if(i == 1){
+        divElem.classList.add("bg-orange");
+    } else {
+        divElem.classList.add("bg-blue");
+    }
+    switch (numberSymbol){
+        case 1:
+            divElem.innerText = "C";
+            break;
+        
+        case 2:
+            divElem.innerText = "+";
+            break;
+    
+        case 3:
+            divElem.innerText = "-";
+            break;
+
+        case 4:
+            divElem.innerText = "x";
+            break;
+
+        case 5:
+            divElem.innerText = "/";
+            break;
+    }
+
+    // divElem.addEventListener("click", operation);
+    return divElem;
+}
--- js/script.js
+++ js/script.js
@@ -1,8 +1,17 @@
 let operations = 0;
 divNumberElem = document.querySelector(".number");
-// create number{
+divSymbolElem = document.querySelector(".symbol");
+
+// create number
     for(i=1; i<=11; i++){
         const newNumber = createNumber(i);
         console.log(newNumber);
         divNumberElem.append(newNumber);
+    }
+
+//  create symbol
+    for(i=1; i<=5; i++){
+        const newSymbol = createSymbol(i);
+        console.log(newSymbol);
+        divSymbolElem.append(newSymbol);
     }
