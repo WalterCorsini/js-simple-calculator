f5509be551080da2ffc0bdd75abaf572148192a7
WalterCorsini <corsini.walter@gmail.com>
2024-04-08 22:57:17 +0200

    add: some function but don't work

--- js/function.js
+++ js/function.js
@@ -3,23 +3,21 @@
  * @param {number} number
  * @returns {object} element HTML
  */
-function createNumber(number){
-    const divElem = document.createElement("div");
-    divElem.classList.add("numbers");
+function createNumber(number) {
+  const divElem = document.createElement("button");
+  divElem.classList.add("numbers");
+  divElem.innerText = number;
+  if (number < 10) {
+    divElem.classList.add("bg-grey", "btn-number");
     divElem.innerText = number;
-    if(number < 10) {
-        divElem.classList.add("bg-grey");
-        divElem.innerText = number;
-    } else if (number == 10){
-        divElem.classList.add("bg-grey", "special");
-        divElem.innerText = "0";
-    } else if (number === 11){
-        divElem.classList.add("bg-orange");
-        divElem.innerText = "=";      
-    }
-
-    // divElem.addEventListener("click", operation);
-    return divElem;
+  } else if (number == 10) {
+    divElem.classList.add("bg-grey", "btn-number", "special");
+    divElem.innerText = "0";
+  } else if (number === 11) {
+    divElem.classList.add("bg-orange", "btn-total");
+    divElem.innerText = "=";
+  }
+  return divElem;
 }
 
 /**
@@ -27,36 +25,64 @@ function createNumber(number){
  * @param {number} numberSymbol
  * @returns {object}  element HTML
  */
-function createSymbol(numberSymbol){
-    const divElem = document.createElement("div");
-    divElem.classList.add("symbols");
-    if(i == 1){
-        divElem.classList.add("bg-orange");
-    } else {
-        divElem.classList.add("bg-blue");
-    }
-    switch (numberSymbol){
-        case 1:
-            divElem.innerText = "C";
-            break;
-        
-        case 2:
-            divElem.innerText = "+";
-            break;
-    
-        case 3:
-            divElem.innerText = "-";
-            break;
+function createSymbol(numberSymbol) {
+  const divElem = document.createElement("button");
+  divElem.classList.add("symbols", "btn-symbol");
+  if (i == 1) {
+    divElem.classList.add("bg-orange");
+  } else {
+    divElem.classList.add("bg-blue");
+  }
+  switch (numberSymbol) {
+    case 1:
+      divElem.innerText = "C";
+      divElem.classList.add("reset");
+      break;
 
-        case 4:
-            divElem.innerText = "x";
-            break;
+    case 2:
+      divElem.innerText = "+";
+      divElem.classList.add = "add";
+      break;
 
-        case 5:
-            divElem.innerText = "/";
-            break;
-    }
+    case 3:
+      divElem.innerText = "-";
+      divElem.classList.add = "less";
+      break;
 
-    // divElem.addEventListener("click", operation);
-    return divElem;
+    case 4:
+      divElem.innerText = "x";
+      divElem.classList.add = "moltpli";
+      break;
+
+    case 5:
+      divElem.innerText = "/";
+      divElem.classList.add = "divider";
+      break;
+  }
+
+  return divElem;
 }
+
+function btnNumber() {
+  let value;
+  if (divDisplayElem.innerText == "0") {
+    divDisplayElem.innerText = this.innerText;
+  } else {
+    value = divDisplayElem.innerText;
+    divDisplayElem.innerText += this.innerText;
+  }
+  return divDisplayElem.innerText;
+}
+
+function btnOpe(valuee) {
+  const firstValue = valuee + this.innerText;
+  divDisplayElem.innerHTML = "";
+  console.log(firstValue.innerText, "firstValue");
+  return firstValue;
+}
+
+// function btnTotal(valore1, valore2, ope) {
+//   console.log(typeof valore1, typeof valore2, typeof operator);
+//   totalBtnOpe = parseInt(valore1) + ope + parseInt(valore2);
+//   console.log(totalBtnOpe);
+// }
--- js/script.js
+++ js/script.js
@@ -1,17 +1,39 @@
-let operations = 0;
-divNumberElem = document.querySelector(".number");
-divSymbolElem = document.querySelector(".symbol");
+const divNumberElem = document.querySelector(".number");
+const divSymbolElem = document.querySelector(".symbol");
+let divDisplayElem = document.getElementById("display");
+let valore1 = "";
+let valore2 = "";
+let cont = 0;
 
 // create number
-    for(i=1; i<=11; i++){
-        const newNumber = createNumber(i);
-        console.log(newNumber);
-        divNumberElem.append(newNumber);
-    }
+for (i = 1; i <= 11; i++) {
+  const newNumber = createNumber(i);
+  console.log(newNumber);
+  divNumberElem.append(newNumber);
+}
 
 //  create symbol
-    for(i=1; i<=5; i++){
-        const newSymbol = createSymbol(i);
-        console.log(newSymbol);
-        divSymbolElem.append(newSymbol);
-    }
+for (i = 1; i <= 5; i++) {
+  const newSymbol = createSymbol(i);
+  console.log(newSymbol);
+  divSymbolElem.append(newSymbol);
+}
+
+const btnNumberArray = document.querySelectorAll(".btn-number");
+const btnSymbolArray = document.querySelectorAll(".btn-symbol");
+
+//  btn click number
+for(let i = 0; i <= 9; i++){
+    // if(cont = 0){
+    valore1 = btnNumberArray[i].addEventListener("click", btnNumber);
+    // } else if(cont = 1){
+    // }
+}
+
+for(let j = 1; j < 5; j++){
+    btnSymbolArray[j].addEventListener("click", btnOpe(valore1));
+}
+
+
+
+
